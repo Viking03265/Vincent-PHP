7b180948d9d06570ec762c424f5278ae8146d024
Vitalii Koshara <86851008+viking-koshara@users.noreply.github.com>
2021-08-09 11:23:23 +0300

    08-09-2021

--- views/contents.php
+++ views/contents.php
@@ -11,35 +11,11 @@
     </div>
     <!-- End of Page Wrapper -->    
 
-    <?php include_once ('Home\\footer.php') ?>    
+    <?php include_once ('Home\\footer.php') ?>
 
     <?php include_once ('Home\\popup.php') ?>    
-
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
     
+    <?php include_once ('Home\\others.php') ?>   
 
     <!-- Bootstrap core JavaScript-->
     <script src="assets/vendor/jquery/jquery.min.js"></script>
